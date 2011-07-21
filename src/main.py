d68aefbf974149f1b1386421bfc0484e75a70632
Tommi Hyppänen <thyppane@gmail.com>
2011-07-21 07:54:14 +0300

    All tests passed

--- src/main.py
+++ src/main.py
@@ -65,128 +65,114 @@ def interpret(prg):
         else:             code.append(i)
     return code[::-1]
 
-stack = []
 # 0 [] "" {} = false, everything else = true
+
 def _false(a):
     return a == ('i', 0) or a == ('a', []) or a == ('s', '') or a == ('b', [])
 def _true(a):
     return not _false(a)
 
-def _push(a): stack.append(a)
-def _pop():   return stack.pop()
+def _quote(a):
+    logging.debug(a)
+    def ww(i):
+        if i[0] == 'i': return repr(i[1])
+        if i[0] == 's': return i[1] #'\"' + i[1] + '\"'
+        if i[0] == 'w': return i[1]
+        if i[0] == 'a': return "[" + ' '.join([ww(x) for x in i[1]]) + "]"
+        if i[0] == 'b': return '{' + ''.join([ww(x) for x in i[1]]) + '}'
+    if   type(a) == type([]): t = ' '.join([ww(x) for x in a])
+    elif type(a) == type(()): t = ww(a)
+    return ('s', t)
 
 def funcs():
-    global stack
-
     def i_bitwise(a): return ('i', ~a[0][1])
     def s_bitwise(a): raise ValueError("Unimplemented.")
     def b_bitwise(a): raise ValueError("Unimplemented.")
-    def a_bitwise(a):  
-        for x in a[0][1]: _push(x)
+    def a_bitwise(a): return a
 
-    def i_dollar(a,b): raise ValueError("Unimplemented.")
+    def i_dollar(a): raise ValueError("Unimplemented.")
     def a_dollar(a): raise ValueError("Unimplemented.")
-    def b_dollar(a,b): raise ValueError("Unimplemented.")
+    def b_dollar(a): raise ValueError("Unimplemented.")
 
-    def i_i_plus(a): return ('i', a[0][1]+a[1][1])
-    def a_a_plus(a): return ('a', a[1][1]+a[0][1])
-    def b_b_plus(a): return ('b', a[1][1]+a[0][1])
+    def i_i_plus(a,b): return ('i', a+b)
+    def a_a_plus(a,b): return ('a', b+a)
+    def b_b_plus(a,b): return ('b', b+a)
 
-    def i_i_sub(a): return ('i', a[1][1]-a[0][1])
+    def i_i_sub(a,b): return ('i', b-a)
 
-    def i_i_mul(a): return ('i', a[0][1]*a[1][1])
-    def b_i_mul(a): raise ValueError("Unimplemented.")
-    def a_i_mul(a): raise ValueError("Unimplemented.")
-    def a_a_mul(a): raise ValueError("Unimplemented.")
-    def a_b_mul(a):
-        x = a[1][1]
+    def i_i_mul(a,b): return ('i', a*b)
+    def b_i_mul(a,b): raise ValueError("Unimplemented.")
+    def a_i_mul(a,b): raise ValueError("Unimplemented.")
+    def a_a_mul(a,b): raise ValueError("Unimplemented.")
+    def a_b_mul(a,b):
+        x = b
         while len(x)>1:
-            b,c = x.pop(),x.pop()
-            logging.debug(repr(b)+repr(c))
-            cm = a[0][1][::-1]+[b]+[c]
-            logging.debug(""+repr(cm))
+            i,j = x.pop(),x.pop()
+            cm = a[::-1]+[i]+[j]
             x.append(exec_ast(cm, [])[0])
         return x[0]
 
-    def i_i_each(a): return ('i', a[1][1]/a[0][1])
+    def i_i_each(a,b): return ('i', b/a)
     def a_a_each(a): raise ValueError("Unimplemented.")
     def a_i_each(a): raise ValueError("Unimplemented.")
     def b_b_each(a): raise ValueError("Unimplemented.")
     def a_b_each(a): raise ValueError("Unimplemented.")
 
-    def i_i_mod(a): return ('i', a[0][1]%a[1][1])
-    def a_a_mod(a): raise ValueError("Unimplemented.")
-    def a_i_mod(a): return ('a', a[1][1][::a[0][1]])
+    def i_i_mod(a,b): return ('i', b%a)
+    def a_a_mod(a,b): raise ValueError("Unimplemented.")
+    def a_i_mod(a,b): return ('a', b[::a])
 
-    def a_b_mod(a):
+    def a_b_mod(a,b):
         x = []
-        for i in a[1][1]:
-            cm = [i]+a[0][1]
+        for i in b:
+            cm = [i]+a
             x.append(exec_ast(cm[::-1], [])[0])
         return ('a', x)
 
-    def i_comma(a): return ("a", [('i', x) for x in range(a[0][1])])
-
-    def i_i_poww(a): return ('i', a[1][1]**a[0][1])
-    def i_a_poww(a):
-        for i,j in enumerate(a[0][1]): 
-            logging.debug("i_a_poww(): "+repr(i)+repr(j)+repr(a[1]))
-            if a[1][0] == j[0] and a[1][1] == j[1]: return i
-        return('i', -1)
-    def a_b_poww(a):
-        for i in a[1][1]:
-            r = exec_ast(a[0][1][::-1]+[i], stack)
-            _pop()
+    def i_i_poww(a,b): return ('i', b**a)
+    def i_a_poww(a,b):
+        for i,j in enumerate(b): 
+            if i == j[0]: return i
+        return ('i', -1)
+    def a_b_poww(a,b):
+        for i in b:
+            r = exec_ast(a[::-1]+[i], [])
             if r[0] == ('i', 1): return i
 
-    def i_inc(a): return ('i', a[0][1]+1)
-    def i_dec(a): return ('i', a[0][1]-1)
+    def i_i_lessert(a,b): return ('i', 0 if a<b else 1)
+    def a_i_lessert(a,b): return ('a', [i for i in b if i[1]<a])
+    def i_i_greatert(a,b): return ('i', 0 if a>b else 1)
+    def a_i_greatert(a,b): return ('a', [i for i in b if i[1]>=a])
 
-    def i_i_lessert(a): return ('i', 0 if a[0][1]<a[1][1] else 1)
-    def a_i_lessert(a): return ('a', [i for i in a[1][1] if i[1]<a[0][1]])
-
-    def i_i_greatert(a): return ('i', 0 if a[0][1]>a[1][1] else 1)
-    def a_i_greatert(a): return ('a', [i for i in a[1][1] if i[1]>=a[0][1]])
- 
-    def b_doo(a): 
-        while True:
-            exec_ast(a[0][1][::-1], stack)
-            if not _true(_pop()): break
+    def i_comma(a): return ('a', [('i', x) for x in range(a)])
+    def i_inc(a): return ('i', a+1)
+    def i_dec(a): return ('i', a-1)
     
-    def swap(): a=_pop(); b=_pop(); _push(a); _push(b);
-    def drop(): 
-        if stack: stack.pop()
-    def dup(): a = stack.pop(); stack.append(a); stack.append(a)
-    def exlamation(a): return ('i',1-a[1])
-    def rot3(): a=_pop(); b=_pop(); c=_pop(); _push(a); _push(c); _push(b);  
-    def quote(): 
-        a = _pop()
-        logging.debug("quote():"+repr(a))
-        def ww(i):
-            if i[0] == 'i': return repr(i[1])
-            if i[0] == 's': return i[1] #'\"' + i[1] + '\"'
-            if i[0] == 'w': return i[1]
-            if i[0] == 'a': return "[" + ' '.join([ww(x) for x in i[1]]) + "]"
-            if i[0] == 'b': return '{' + ''.join([ww(x) for x in i[1]]) + '}'
-            
-        if type(a) == type([]): t = ' '.join([ww(x) for x in a])
-        else: t = ww(a)
-        logging.debug("quote():"+repr(t))
-        return t
+    def i_exlamation(a): return ('i',1-a)
     
-    def none(): return None
-
+    def swap(a,b): return [a,b]
+    def dup(a): return [a,a]
+    def drop(a): return None
+    
+    def rot3(a,b,c): return [b,a,c] 
+    def quote(a): return _quote(a)
+    def none(): pass
     def bracko(): return ('w','[')
-    def bracke():
+    def pputs(): print quote()[1]
+
+    # -- functions that needs full stack
+    def b_doo(a,s): 
+        while True:
+            exec_ast(a[::-1], s)
+            if not _true(s.pop()): break
+    
+    def bracke(s):        
         l = []
-        while stack and stack[-1][1] != '[':
-            l.append(stack.pop())
-        stack.pop()
-        stack.append(('a', l[::-1]))
-        
-    def pputs(): 
-        print quote()
-        
+        while s and s[-1][1] != '[':
+            l.append(s.pop())
+        if s and s[-1][1] == '[': s.pop()
+        s.append(('a', l[::-1]))            
+  
     return locals()
 
 words = {}
@@ -197,19 +183,18 @@ for k,v in funcs().iteritems():
         words[n][t] = v
     else:
         words[n] = {t:v}
+        
+# words that need the whole stack
+stackwords = set(['bracke', 'doo'])
 
 def exec_ast(c, st):
-    logging.debug("exec_ast():"+repr(c))
+    logging.debug("exec_ast():"+_quote(c[::-1])[1]+repr(st))
     while c:
         i = c.pop()
         if i[0] == "w":
             # operator
             tm = tokmap[i[1]]
             ks = words[tm].keys()
-            
-            if ks != ['']: # skip printing no-argument words
-                logging.debug(">> stack:"+repr(st))
-                logging.debug(">> word:"+repr(i[1])+"="+repr(tm)+repr(ks))
 
             mm = False
             if ks[0] != '':
@@ -220,61 +205,80 @@ def exec_ast(c, st):
                         if t == k:     
                             # match found
                             mm = True
-                            logging.debug("match:"+tm+" | type:"+t)
+                            
                             sp = []
-                            for _ in range(len(t)):
-                                sp.append(st.pop())
-                            r = words[tm][t](sp)
+                            for _ in range(len(t)): sp.append(st.pop()[1])
+                            if tm in stackwords: sp.append(st)
+
+                            logging.debug("exec:"+tm+repr(sp)+repr(st))   
+                            r = words[tm][t](*sp)
                             if r: 
-                                st.append(r)
+                                if type(r)==type([]): st.extend(r)
+                                else: st.append(r)
                                 logging.debug("PUSH:"+repr(r))
                             break
                     else:
-                        raise ValueError("Stack underflow.")
+                        raise ValueError("Stack underflow. ",st)
                 if not mm: raise ValueError("Unable to match instruction:"+
                                             repr(tm)+","+repr(ks)+","+
                                             repr([x[0] for x in st[-3:]]))
             else:
-                # words with no defined types
-                r = words[tm]['']()
-                logging.debug("Executed:"+repr(tm))
+                # words with no types
+                sp = []
+                wl = len(inspect.getargspec(words[tm][''])[0])
+                if tm in stackwords: sp.append(st)
+                else:
+                    for _ in range(wl): sp.append(st.pop())
+                
+                
+                    
+                logging.debug("exec:"+tm+repr(sp)+repr(st))    
+                r = words[tm][''](*sp)
+
                 if r: 
-                    logging.debug("append:"+repr(r))
-                    st.append(r)
+                    if type(r)==type([]): st.extend(r)
+                    else: st.append(r)
+                    logging.debug("a:"+repr(r))
+
         else:
             # not a word, just add to stack
             st.append(i)
-    logging.debug("exit:"+repr(stack))
     return st[:]
       
 def run_tests():
-    global stack
-    tests = {
-             """5 2,~{..p@+.100<}do""":"",
-             """[1 2 3 4]{+}*""":"",
-             """2 4*""":"",
-             """7 3 /""":"",
-             """5 2 ?""":"",
-             """[1 2 3 4 5 6]{.* 20>}?""":"",
-             """5`""":"",
-             """[1 2 3]`""":"",
-             """{[2 3 4] 5 3 6 {.@\%.}*}`""":"",
-             """ 5,`""":"",
-             """2706 410{.@\%.}do;""":"",
-             """5{1-..}do""":"",
-             """5 1+,1>{*}*""":"",
-             """ 3 4 >""":"",
-             """[1 2 3][4 5]+""":"",
-             """1 1+""":"",
-             """5,{1+}%{*}*""":""
-             }
+    tests = [
+             ("""3 2.""","3 2 2"),
+             ("""[3 2].[5]""","[3 2] [3 2] [5]"),
+             ("""1 2 3@""","2 3 1"),
+             ("""1 2\\""","2 1"),
+             ("""1 1+""","2"),
+             ("""2 4*""","8"),
+             ("""7 3 /""","2"),
+             ("""5 2 ?""","25"),
+             ("""5`""","5"),
+             ("""[1 2 3]`""","[1 2 3]"),
+             (""" 3 4 >""","0"),
+             ("""[1 2 3 4]{+}*""","10"),
+             ("""{[2 3 4] 5 3 6 {.@\%.}*}`""","{[2 3 4] 5 3 6 {.@\%.}*}"),
+             (""" 5,`""","[0 1 2 3 4]"),
+             ("""[1 2 3 4 5 6]{.* 20>}?""","5"),
+             ("""5 1+,1>{*}*""","120"),
+             ("""[1 2 3][4 5]+""","[1 2 3 4 5]"),
+             ("""5{1-..}do""","4 3 2 1 0 0"),
+             ("""2706 410{.@\%.}do;""","82"),
+             ("""5 2,~{.@+.100<}do""","5 89 144"),
+             ("""5,{1+}%{*}*""","120")
+             
+             ]
     
-    for i,j in tests.iteritems():
+    for it in tests:
         try:
             stack = []
-            print i,"=>\t\t",exec_ast(interpret(i), stack),j
-        except:
-            print "FAIL:",i,j
+            res = _quote(exec_ast(interpret(it[0]), stack))[1]
+            if it[1]==res: print "SUCC:",it[0],"=>",res
+            else: print "FAIL:",it[0],"=>",res
+        except a:
+            print "FAIL:",it[0],it[1]
 
 run_tests()           
-#print exec_ast(interpret(program), stack)
+#print exec_ast(interpret("""2706 410{.@\%.}do;"""), [])
